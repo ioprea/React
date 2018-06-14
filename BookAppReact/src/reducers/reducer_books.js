1ed424d7862a4734a153ee2ea62bf6987ec10b13
Oprea Ionut <Johnny@Johnnys-MacBook-Pro.local>
2018-06-14 17:29:57 +0300

    v3

---
+++ BookAppReact/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "Harry Potter", pages: 101},
+        {title: "Javascript", pages: 44},
+        {title: "Python", pages: 24},
+        {title: "React", pages: 12}
+    ];
+}
